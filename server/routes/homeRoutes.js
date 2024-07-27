130b95d07e5ecdd4b945b9763800fe481402e7f2
LeVente Berry <berrylevente@gmail.com>
2024-07-27 12:08:55 -0400

    working login and signup

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -3,14 +3,23 @@ import jwt from 'jsonwebtoken';
 import { User } from '../models/index.js';
 const routes = express.Router();
 
-// TODO: Signup Route
 
-routes.use('/register', (req, res) => {
-    const { username, password } = req.body;
+routes.use('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        if (username && email && password) {
+            const user = await User.create({username: username, email: email, password: password});
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.json({ token });
+        } else {
+            res.status(403).json({message: "Please complete all fields before submitting"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
 });
 
-
-// TODO: Login Route
 
 routes.use('/login', async (req, res) => {
     try {
@@ -22,17 +31,17 @@ routes.use('/login', async (req, res) => {
             res.status(500).json({ message: "Username or Password Incorrect!" });
         }
 
-        const isPasswordValid = await User.checkPassword(password);
+        const isPasswordValid = await user.checkPassword(password);
+
         if (!isPasswordValid) {
             return res.status(401).send('Invalid password');
+        } else {
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.json({ token });
         }
 
-        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
-
-        res.json({ token });
-
     } catch (error) {
-        res.status(500).json({ message: error })
+        res.status(500).json({ message: error });
         throw error;
     }
 
